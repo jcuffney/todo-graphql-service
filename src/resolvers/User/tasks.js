a12759bb9b316f012a62a78f3a789a63cb18bea4
Joseph Cuffney <josephcuffney@gmail.com>
2024-02-29 22:29:12 -0500

    User.tasks

--- src/resolvers/User/tasks.js
+++ src/resolvers/User/tasks.js
@@ -1,1 +1,31 @@
-export default () => ({ edges: [], pageInfo: { hasNextPage: false, hasPreviousPage: false } })
+import { applyTo, pipe, pick } from "ramda"
+import { serializeKey, parseKey } from '../../lib/string.js';
+import { map } from "../../lib/iterable.js"
+import {
+    nodeToEdgeWith, relayEdgesConnection
+} from '../../lib/graphql.js';
+
+const getNodeCursor = pipe(pick(['id']), serializeKey);
+
+export default (_, { input: { first, after } = {} }, { Task, user }) => {
+    const userId = user?.id;
+
+    return applyTo({
+        IndexName: 'by-userId',
+        KeyConditionExpression: '#userId = :userId',
+        ExpressionAttributeNames: {
+            '#userId': 'userId',
+        },
+        ExpressionAttributeValues: {
+            ':userId': userId,
+        },
+        ExclusiveStartKey: parseKey(after) ? {
+            ...parseKey(after),
+            userId,
+        } : undefined,
+    }, pipe(
+        Task.queryIterator,
+        map(nodeToEdgeWith(getNodeCursor)),
+        relayEdgesConnection({ after, first }),
+    ))
+}
--- src/seed/index.js
+++ src/seed/index.js
@@ -5,6 +5,7 @@ console.log('seeding the database with some data...');
 
 const {
     User,
+    Task,
 } = await getContext({ req: {} })();
 
 await User.put({
@@ -17,5 +18,36 @@ await User.put({
     updatedAt: new Date().toISOString(),
     deletedAt: null,
 })
+
+await Task.put({
+    id: toGlobalId('Task', "1"),
+    title: "task 1",
+    description: "description 1",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    deletedAt: null,
+    userId: toGlobalId('User', "1"),
+})
+
+await Task.put({
+    id: toGlobalId('Task', "2"),
+    title: "task 2",
+    description: "description 2",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    deletedAt: null,
+    userId: toGlobalId('User', "1"),
+})
+
+await Task.put({
+    id: toGlobalId('Task', "3"),
+    title: "task 3",
+    description: "description 3",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    deletedAt: null,
+    userId: toGlobalId('User', "1"),
+})
+
 
 console.log('done.');
